628a70cf2d12f722c185256762c4577f72a26010
Jonathan Hechenleitner <jonathan.hechenleitner@gat-blac.com>
2022-01-24 19:41:10 -0300

    Firsst Commit

---
+++ network/load.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+curl -s -X POST \
+  http://localhost:3000/channels/create \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"MainOrg",
+	"channel":"main-channel",
+	"configPath":"../network/devchannel/channels/main-channel.tx"
+}'
+
+echo
+echo
+sleep 1
+
+curl -s -X POST \
+  http://localhost:3000/channels/create \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"MainOrg",
+	"channel":"allorgs-channel",
+	"configPath":"../network/devchannel/channels/allorgs-channel.tx"
+}'
+
+echo
+echo
+sleep 1
+
+curl -s -X POST \
+  http://localhost:3000/channels/join \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"MainOrg",
+	"channel":"main-channel"
+}'
+
+echo
+echo
+sleep 1
+
+curl -s -X POST \
+  http://localhost:3000/channels/join \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"MainOrg",
+	"channel":"allorgs-channel"
+}'
+
+echo
+echo
+sleep 1
+
+curl -s -X POST \
+  http://localhost:3000/channels/join \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"Org1",
+	"channel":"allorgs-channel"
+}'
+
+echo
+echo
+sleep 1
+
+curl -s -X POST \
+  http://localhost:3000/channels/join \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"Org2",
+	"channel":"allorgs-channel"
+}'
+
+echo
+echo
+sleep 1
+
+curl -s -X POST \
+  http://localhost:3000/smartcontract/install \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"orgs":["MainOrg"],
+	"channel":"main-channel",
+	"chaincode":"test_node"
+}'
+
+curl -s -X POST \
+  http://localhost:3000/smartcontract/install \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"orgs":["MainOrg"],
+	"channel":"main-channel",
+	"chaincode":"smart_licenses"
+}'
+
+echo
+echo
+sleep 1
+
+curl -s -X POST \
+  http://localhost:3000/smartcontract/install \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"orgs":["MainOrg", "Org1", "Org2"],
+	"channel":"allorgs-channel",
+	"chaincode":"test_golang"
+}'
+
+echo
+echo
+sleep 5
+
+curl -s -X POST \
+  http://localhost:3000/smartcontract/query \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"MainOrg",
+	"channel":"main-channel",
+	"chaincode":"test_node",
+	"function":"ping",
+	"args":[]
+}'
+
+echo
+echo
+sleep 5
+
+curl -s -X POST \
+  http://localhost:3000/smartcontract/query \
+  -H "cache-control: no-cache" \
+  -H "content-type: application/json" \
+  -d '{
+	"org":"MainOrg",
+	"channel":"allorgs-channel",
+	"chaincode":"test_golang",
+	"function":"ping",
+	"args":[]
+}'
+
+# Restart Hyperledger Explorer now
+docker-compose up -d
